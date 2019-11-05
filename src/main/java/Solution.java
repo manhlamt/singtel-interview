ed79f1d062de16362f8e8facb2efc36c43729343
Lam Tran <manhlamt@gmail.com>
2019-11-05 23:08:18 +0700

    Question 1: Implement sing method. I do not think there's any optimization needed at this stage where the code is overly simple.

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,6 +2,7 @@ import domain.Bird;
 
 public class Solution {
     public static void main (String[] args) {
+        System.out.println("Question 1: ");
         Bird bird = new Bird();
         bird.walk();
         bird.fly();
---
+++ src/test/java/BirdTest.java
@@ -0,0 +1,27 @@
+import domain.Bird;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+public class BirdTest {
+    private PrintStream out;
+
+    @Before
+    public void setup() {
+        out = mock(PrintStream.class);
+        System.setOut(out);
+    }
+
+    @Test
+    public void testSing_shouldPrintlnCorrectly() {
+        final Bird bird = new Bird();
+        bird.sing();
+        verify(out).println(eq("I'm singing ... chirp chirp !!!"));
+    }
+}
