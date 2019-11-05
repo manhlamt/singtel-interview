9f8504bfb1728b4addc2cc2264a3cf35250329e7
Lam Tran <manhlamt@gmail.com>
2019-11-05 22:59:55 +0700

    Init with IDEA gradle template and given snippet

---
+++ src/main/java/Solution.java
@@ -0,0 +1,10 @@
+import domain.Bird;
+
+public class Solution {
+    public static void main (String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
